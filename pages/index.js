f798fbf1aa7db1fc8ff716443599d595b262d17c
Tri <trinhphanminhtri@gmail.com>
2023-01-05 14:43:34 +0700

    1. Setup project
    - Making global variables accessible in next config

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function HomePage() {
   return (
