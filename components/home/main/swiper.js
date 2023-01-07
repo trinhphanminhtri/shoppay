5faa7ab60fffc4f74ee93e599562efb6aa23bf62
Tri <trinhphanminhtri@gmail.com>
2023-01-07 17:27:08 +0700

    7. Home page
    -- Add autoplay from docs swiperjs.com/demos#autoplay
    -- Finished swiper

--- components/home/main/swiper.js
+++ components/home/main/swiper.js
@@ -8,7 +8,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 import Image from "next/Image";
 
@@ -19,11 +19,15 @@ export default function MainSwiper() {
         slidesPerView={1}
         spaceBetween={30}
         loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation]}
         className="mainSwiper"
       >
         {[...Array(10).keys()].map((item) => (
