c204c715cf119aace7d383484e352f2de72dde19
Tri <trinhphanminhtri@gmail.com>
2023-01-07 17:10:38 +0700

    7. Home page
    -- Home swiper part 1
    -- npm install swiper
    -- swiperjs.com

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -1,11 +1,12 @@
 import styles from "./home-main.module.scss";
+import MainSwiper from "./swiper";
 
 const Main = () => {
   return (
     <div className={styles.main}>
       <div className={styles.header}>header</div>
       <div className={styles.menu}>menu</div>
-      <div className={styles.swiper}>swiper</div>
+      <MainSwiper/>
       <div className={styles.offers}>offers</div>
       <div className={styles.user}>user</div>
     </div>
---
+++ components/home/main/swiper.js
@@ -0,0 +1,42 @@
+import { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+import Image from "next/Image";
+
+export default function MainSwiper() {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mainSwiper"
+      >
+        {[...Array(10).keys()].map((item) => (
+          <SwiperSlide key={item.index}>
+            <Image
+              src={`/images/swiper/${item + 1}.jpg`}
+              alt=""
+              width={750}
+              height={300}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
